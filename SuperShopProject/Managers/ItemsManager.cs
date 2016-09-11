afc9751e7df8abb227c1caf494cad42e79c691f7
Turlei Roman <r.turlei.rt@gmail.com>
2016-09-12 01:03:00 +0300

    begining of items

---
+++ SuperShopProject/Managers/ItemsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SuperShopProject.Models;
+
+namespace SuperShopProject.Managers
+{
+    public class ItemsManager: BaseManager
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemsManager(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
+
+        public async Task<ICollection<Item>> GetAll()
+        {
+            return await _db.Items.ToListAsync();
+        }
+
+        public async Task<Item> GetById(Guid id)
+        {
+            return await _db.Items.FindAsync(id);
+        }
+
+        public async Task Add(Item item)
+        {
+            item.Id = Guid.NewGuid();
+            _db.Items.Add(item);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Save(Item item)
+        {
+            var tochange = await _db.Items.FindAsync(item.Id);
+            if (tochange != null)
+            {
+                tochange.Count = item.Count;
+                tochange.Description = item.Description;
+                tochange.Name = item.Name;
+                tochange.Price = item.Price;
+                tochange.Type = item.Type;
+            }
+            await _db.SaveChangesAsync();
+        }
+    }
+}
